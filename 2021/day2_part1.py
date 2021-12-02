fd9e4bdd3d813b22976c852e334e1ceb4c869c0c
T-Zocker <tzoockeer@gmail.com>
2021-12-02 19:20:51 +0100

    Added day 2 part 1

---
+++ 2021/day2_part1.py
@@ -0,0 +1,16 @@
+import re
+
+with open("2021/inputs/day2_input.txt") as file:
+    input = file.readlines()
+
+horizontal_pos = 0
+depht = 0
+for line in input:
+    if line.find("forward") == 0:
+        horizontal_pos += int(re.findall("[0-9]", line)[0]) # Finds the number
+    elif line.find("down") == 0:
+        depht += int(re.findall("[0-9]", line)[0]) # Finds the number
+    else:
+        depht -= int(re.findall("[0-9]", line)[0]) # Finds the number
+
+print("Answer: " + str(horizontal_pos * depht))
